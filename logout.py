4563d13970822fc36875b205833c6f60a1c997c8
Nthniell <nathanielliady08@gmail.com>
2023-04-10 16:05:30 +0700

    f01 dan f02

---
+++ logout.py
@@ -0,0 +1,6 @@
+if sesi == []:
+                print("Logout gagal!")
+                print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+            else:
+                print("Logout berhasil")
+                sesi = []

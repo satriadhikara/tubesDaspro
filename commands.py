1dccd3f79921ccd27845680b1b00e9e08d86550f
satriadhikara <satriadhikara@gmail.com>
2023-04-14 13:57:19 +0700

    updates

--- commands.py
+++ commands.py
@@ -156,15 +156,6 @@ def ubahjin(user, banyakData):
                         print("Jin telah berhasil diubah.")
                     elif validasi == "N" or validasi == 'n':
                         user[i][2] = user[i][2]
-                while True:
-                    validasi = input(
-                        "Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?")
-                    if validasi == "Y" or validasi == 'y':
-                        user[i][2] = "pembangun"
-                        print("Jin telah berhasil diubah.")
-                    elif validasi == "N" or validasi == 'n':
-                        user[i][2] = user[i][2]
-                    return user
             elif user[i][2] == "pembangun":
                 while True:
                     validasi = input(
@@ -174,15 +165,6 @@ def ubahjin(user, banyakData):
                         print("Jin telah berhasil diubah.")
                     elif validasi == "N" or validasi == 'n':
                         user[i][2] = user[i][2]
-                while True:
-                    validasi = input(
-                        "Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)?")
-                    if validasi == "Y" or validasi == 'y':
-                        user[i][2] = "pengumpul"
-                        print("Jin telah berhasil diubah.")
-                    elif validasi == "N" or validasi == 'n':
-                        user[i][2] = user[i][2]
-                    return user
     else:
         print("Tidak ada jin dengan username tersebut.")
         return user
